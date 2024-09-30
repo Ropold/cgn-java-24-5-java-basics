2362ec9de8713dd4aa8ad25f00b30f5209bb9269
Robert Stolz <robert.stolz87@gmail.com>
2024-09-30 12:04:25 +0200

    whenW_Yequals1

--- src/main/java/PlayerCharacter.java
+++ src/main/java/PlayerCharacter.java
@@ -3,5 +3,16 @@ public class PlayerCharacter {
     public static int getX(){
        return 0;
     }
+    public static int getY(){
+        return 0;
+    }
+
+    public static int move(String input){
+        if(input == "W"){
+            return 1;
+        } else {
+            return 0;
+        }
+    }
 
 }
--- src/test/java/PlayerCharacterTest.java
+++ src/test/java/PlayerCharacterTest.java
@@ -13,4 +13,28 @@ public class PlayerCharacterTest {
         //THEN
         assertEquals(0, actual);
     }
+    @Test
+    public void getY_return0 (){
+        //GIVEN
+
+        //WHEN
+        int actual = PlayerCharacter.getY();
+
+        //THEN
+        assertEquals(0, actual);
+    }
+
+    @Test
+    public void move_Test_whenW_Yequals1(){
+        //given
+        String input = "W";
+
+        //when
+        int actual = PlayerCharacter.move(input);
+
+        //then
+        assertEquals(1,actual);
+
+    }
 }
+
